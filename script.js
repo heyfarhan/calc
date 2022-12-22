4b33f3c9c2ba2a1cbe30b46c44788764e1a2f82a
heyfarhan <113258806+heyfarhan@users.noreply.github.com>
2022-12-22 20:19:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,28 +6,32 @@ var s;
 
 function btn(e){
     const i=e.target.innerText;
-    
     if(i=='AC'){
+        navigator.vibrate(200);
         console.log(i);
         r.innerText='';
         
     }
     else if(i==undefined){
+        navigator.vibrate(100);
         console.log(i);
         r.innerText=r.innerText.slice(0,-1);
         
     }
     else if(i=='%'){
+        navigator.vibrate(100);
         console.log(eval(r.innerText)/100);
         r.innerText=r.innerText+'/100'
         
     }
     else if(i=='='){
+        navigator.vibrate(100);
         r.innerText=eval(r.innerText);
         
         
     }
     else{
+        navigator.vibrate(100);
         s=r.innerText+i;
         console.log(i)
         r.innerText=s;
@@ -40,16 +44,20 @@ function key(e){
     if(0<=i || i>=9 || i=='.' || i=='+' || i=='-' || i=='*' || i=='/' ){
         s=r.innerText+i;
         r.innerText=s;
+        navigator.vibrate(100);
     }
     else if(i=="%"){
         console.log(eval(r.innerText)/100);
         r.innerText=r.innerText+'/100'
+        navigator.vibrate(100);
     }
     else if(i=='Enter' || i=='='){
         r.innerText=eval(r.innerText);
+        navigator.vibrate(100);
     }
     else if(i=="Backspace"){
         r.innerText=r.innerText.slice(0,-1);
+        navigator.vibrate(100);
         
     }
 }
